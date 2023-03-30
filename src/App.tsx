3ba6a6b0c5ee079134033e9467d0a09e84ce7f39
MsNefskiy <salionk223457162@gmail.com>
2023-03-30 13:13:21 +0300

    src: add icons assets and image

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import createTheme from '@mui/material/styles/createTheme';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Hero } from './components/Hero';
+import { Skills } from './components/Skills';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 function App() {
   return (
-    <div>
-        Привет, React
-    </div>
-  )
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Header />
+        <Hero />
+        <Skills />
+        <Footer />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
