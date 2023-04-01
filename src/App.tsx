5ee1200a23d41ff0881beeed2fc00d8c3b5ce75d
MsNefskiy <salionk223457162@gmail.com>
2023-04-01 16:14:36 +0300

    feature: add switching themes to light and dark

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,87 @@ import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import { Skills } from './components/Skills';
+import { ThemeOptions } from '@mui/material/styles';
+import { createContext, useState } from 'react';
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
+export interface IThemeContext {
+  mode: string;
+  toggleTheme?: () => void;
+}
+
+const defaultState = {
+  mode: localStorage.getItem('darkMode') || 'light',
+};
+
+export const ThemeContext = createContext<IThemeContext>(defaultState);
 
 function App() {
+  const [mode, setMode] = useState(localStorage.getItem('darkMode') || 'light');
+
+  const toggleTheme = () => {
+    setMode(() => (mode === 'dark' ? 'light' : 'dark'));
+  };
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: 'rgba(0,199,255,255)',
+      },
+      secondary: {
+        main: '#a892ff',
+      },
+      background: {
+        default: 'rgba(0, 10, 31, 255)',
+        paper: 'rgba(0, 10, 31, 255)',
+      },
+    },
+  });
+
+  const lightTheme = createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#0a2af3',
+      },
+      secondary: {
+        main: 'rgba(0,199,255,255)',
+      },
+    },
+    components: {
+      MuiAppBar: {
+        styleOverrides: {
+          colorInherit: {
+            backgroundColor: 'rgba(0, 10, 31, 255)',
+            color: '#fff',
+          },
+        },
+      },
+
+      MuiLink: {
+        styleOverrides: {
+          root: {
+            color: 'rgba(0,199,255,255)',
+          },
+        },
+      },
+    },
+  });
+
+  const selectedTheme = mode === 'dark' ? darkTheme : lightTheme;
+
   return (
     <>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <Header />
-        <Hero />
-        <Skills />
-        <Contacts />
-        <Footer />
-      </ThemeProvider>
+      <ThemeContext.Provider value={{mode, toggleTheme}}>
+        <ThemeProvider theme={selectedTheme}>
+          <CssBaseline />
+          <Header />
+          <Hero />
+          <Skills />
+          <Contacts />
+          <Footer />
+        </ThemeProvider>
+      </ThemeContext.Provider>
     </>
   );
 }
