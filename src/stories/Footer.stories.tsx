1320b85e9c11685def8adbc0bc3e74a07161f792
MsNefskiy <salionk223457162@gmail.com>
2023-04-07 12:43:54 +0300

    feature: add footer and header stories

--- src/stories/Footer.stories.tsx
+++ src/stories/Footer.stories.tsx
@@ -1,26 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Header } from './Header';
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
 
 const meta = {
-  title: 'Example/Header',
-  component: Header,
+  title: 'Footer',
+  component: Footer,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/7.0/react/writing-docs/docs-page
   tags: ['autodocs'],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
     layout: 'fullscreen',
   },
-} satisfies Meta<typeof Header>;
+} satisfies Meta<typeof Footer>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const LoggedIn: Story = {
-  args: {
-    user: {
-      name: 'Jane Doe',
-    },
-  },
-};
-
-export const LoggedOut: Story = {};
+export const Example: Story = {};
---
+++ src/stories/Header.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Header } from '../components/Header';
+
+const meta = {
+  title: 'Header',
+  component: Header,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/7.0/react/writing-docs/docs-page
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Header>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {};
